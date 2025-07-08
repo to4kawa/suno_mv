6f708b4baaae6b9d7b0cf6f210b401703b0128ac
Toshikawa <to.4kawa@gmail.com>
2025-07-08 19:18:27 +0900

    preload.jsしゅうせい

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  generateMP4: (params) => ipcRenderer.invoke('generate-mp4', params)
+    generateMP4WithBase64: (params) => ipcRenderer.invoke('generate-mp4-with-base64', params),
+    getFFmpegPath: () => ipcRenderer.invoke('get-ffmpeg-path'),
+    saveFFmpegPath: (newPath) => ipcRenderer.invoke('save-ffmpeg-path', newPath)
 });
